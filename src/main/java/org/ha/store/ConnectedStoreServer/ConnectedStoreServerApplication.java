1e024ef5816327dc12ff6114a6320b3aec674751
hanaharile <hanaharile98@gmail.com>
2019-05-18 00:16:05 +0000

    first commit

---
+++ src/main/java/org/ha/store/ConnectedStoreServer/ConnectedStoreServerApplication.java
@@ -0,0 +1,13 @@
+package org.ha.store.ConnectedStoreServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectedStoreServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectedStoreServerApplication.class, args);
+	}
+
+}
