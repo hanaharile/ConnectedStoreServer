3c1fa8f0687fcd46e654d6aff021ffa1bd44943b
hanaharile <hanaharile98@gmail.com>
2019-05-29 02:25:56 +0100

    changes already are done

--- src/main/java/org/ha/store/ConnectedStoreServer/controller/ProduitController.java
+++ src/main/java/org/ha/store/ConnectedStoreServer/controller/ProduitController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/produits")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 public class ProduitController{
 	
 	@Autowired
